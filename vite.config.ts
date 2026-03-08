8247f736aeb029e1115f0bcc1c2d52d68e330f6c
Tharaniesh <tharaniesh46@gmail.com>
2026-03-08 13:47:47 +0530

    small bug fixes for githost

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const repoBase = '/PortFolio/';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? repoBase : '/',
   build: {
     sourcemap: false,
     target: 'es2020'
