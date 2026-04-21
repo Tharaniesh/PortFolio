7b154ccf42fc830810517bebf512c8b53ad1d029
Tharaniesh <tharaniesh46@gmail.com>
2026-04-21 16:45:17 +0530

    checks for uncommits

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const base = env.VITE_BASE_PATH || '/';
+  const base = env.VITE_BASE_PATH || (mode === 'production' ? './' : '/');
 
   return {
     plugins: [react()],
